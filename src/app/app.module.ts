9685227e1b91893818b20b7b7a47e3bc4a497e4c
sercan mercan <sercan.mercan@dijitalkurye.com.tr>
2022-09-04 22:26:25 +0300

    about ve experince bölümleri eklendi.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,41 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { HeaderComponent } from './profile/header/header.component';
+import { ExperienceComponent } from './profile/experience/experience.component';
+import { SkillsComponent } from './profile/skills/skills.component';
+import { EducationComponent } from './profile/education/education.component';
+import { ProfileModule } from './profile/profile.module';
+import { AppRoutingModule } from './app-routing.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent
+  },
+  {
+    path: '',
+    component : ExperienceComponent,
+  },
+  {
+    path: '',
+    component : SkillsComponent,
+  },
+  {
+    path: '',
+    component : EducationComponent,
+  }
+  
+];
+
+const config: ExtraOptions = {
+  useHash: true,
+};
 
 @NgModule({
   declarations: [
@@ -10,6 +43,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes, config),
+    ProfileModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,18 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+import { SnotifyPosition } from 'ng-snotify';
 export const environment = {
-  production: false
+  production: false,
+  //baseUrl : `${window.location.protocol}//${window.location.hostname}/portfolio/`,
+  baseUrl : `http://localhost:4200/`,
+
+  snotifyConfig : {
+    showProgressBar: true,
+    position: SnotifyPosition.rightTop,
+  },
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
